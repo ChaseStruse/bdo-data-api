5a1748555cc5891f59075cf1d4ba723a3d763b6e
Chase Struse <chasestruse@gmail.ocm>
2024-06-22 21:29:16 -0500

    Added in a post route that will calculate the gear score based on the information sent in

--- Sources/App/Controllers/AdventurerController.swift
+++ Sources/App/Controllers/AdventurerController.swift
@@ -1,19 +1,31 @@
 import Vapor
 import Fluent
 
+struct gearScoreRequest: Content {
+    var ap: Int
+    var dp: Int
+    var aap: Int
+}
+
 struct AdventurerController: RouteCollection {
     func boot(routes: RoutesBuilder) throws {
         let adventurerRoutes = routes.grouped("adventurer")
         
         // /adventurer/gearScore
         adventurerRoutes.group("gearScore") { adventurerRoute in
-            adventurerRoute.get(use: getGearScoreHandler)
+            adventurerRoute.get(use: getGearScore)
+            adventurerRoute.post(use: calculateGearScore)
         }
         
     }
     
-    func getGearScoreHandler(_req: Request) async throws -> String {
+    @Sendable func getGearScore(_req: Request) async throws -> String {
         return "157"
+    }
+    
+    @Sendable func calculateGearScore(_req: Request) async throws -> String {
+        let request = try _req.content.decode(gearScoreRequest.self)
+        return String((request.ap + request.aap)/2 + request.dp)
     }
     
 
